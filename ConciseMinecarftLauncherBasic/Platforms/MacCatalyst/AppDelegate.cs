2e21f509a2a0abac168374eae2cd20e9833e81ec
T0 Hello <helloIT0@outlook.com>
2023-09-23 14:49:47 +0800

    创建项目

---
+++ ConciseMinecarftLauncherBasic/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ConciseMinecarftLauncherBasic;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ConciseMinecarftLauncher
+    简介
+    顾名思义这是一个 Minecraft Java Edition 启动器
